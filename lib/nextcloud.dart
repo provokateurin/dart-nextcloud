a7b19f70fb76301c3fe2e005d58ddab46914961d
jld3103 <jld3103yt@gmail.com>
2019-10-22 23:08:58 +0200

    Export RequestException

--- lib/nextcloud.dart
+++ lib/nextcloud.dart
@@ -1,7 +1,8 @@
 library nextcloud;
 
 export 'package:nextcloud/src/client.dart' show NextCloudClient;
-export 'package:nextcloud/src/webdav/file.dart' show WebDavFile;
-export 'package:nextcloud/src/webdav/client.dart' show WebDavClient;
 export 'package:nextcloud/src/metadata/client.dart' show MetaDataClient;
 export 'package:nextcloud/src/metadata/metadata.dart' show MetaData;
+export 'package:nextcloud/src/network.dart' show RequestException;
+export 'package:nextcloud/src/webdav/client.dart' show WebDavClient;
+export 'package:nextcloud/src/webdav/file.dart' show WebDavFile;
